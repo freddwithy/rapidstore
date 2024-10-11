d971d413c7353d80a4bbec9ee9709c4692ce70b7
freddwithy <134991353+freddwithy@users.noreply.github.com>
2024-10-11 17:04:43 -0300

    Update page.tsx

--- app/(store)/[storeId]/page.tsx
+++ app/(store)/[storeId]/page.tsx
@@ -6,7 +6,7 @@ const Page = ({ params }: { params: { storeId: string } }) => {
     <div className="h-dvh w-full bg-stone-200">
       <Header
         storeName={params.storeId}
-        storeDescription="Tienda de prueba"
+        storeDescription="Tienda de mascotas"
         storeLogo="https://i0.wp.com/digitalhealthskills.com/wp-content/uploads/2022/11/3da39-no-user-image-icon-27.png?fit=500%2C500&ssl=1"
       />
     </div>
