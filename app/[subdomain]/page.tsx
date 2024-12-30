3fc7f24678d832da440d7075a33fb480b06db0f1
Fredd <134991353+freddwithy@users.noreply.github.com>
2024-12-29 23:21:45 -0300

    cambios

--- app/[subdomain]/page.tsx
+++ app/[subdomain]/page.tsx
@@ -1,6 +1,10 @@
+import Instagram from "@/components/icons/instagram";
+import WhatsApp from "@/components/icons/whatsapp";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
 import prismadb from "@/lib/prismadb";
-import { AlertCircle } from "lucide-react";
+
+import { AlertCircle, List } from "lucide-react";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
@@ -17,9 +21,13 @@ export default async function SubdomainPage({
       where: {
         name: subdomain,
       },
+      include: {
+        categories: true,
+      },
     });
 
     console.log("Page: Found store", store);
+    console.log(store?.categories);
 
     if (!store) {
       console.log("Page: Store not found, returning 404");
@@ -28,8 +36,15 @@ export default async function SubdomainPage({
 
     return (
       <div className="w-full mx-auto h-dvh flex flex-col">
-        <div className="h-40 w-full bg-foreground border-b border relativo">
-          <div className="absolute size-32 rounded-lg bg-zinc-100 top-20 left-40 border-foreground border-4 overflow-hidden">
+        <div className="h-40 w-full bg-foreground border-b border relativo overflow-hidden">
+          <Image
+            src="https://i.ebayimg.com/images/g/n8IAAOSwltRkNCSF/s-l1200.png"
+            alt="portada"
+            width={1500}
+            height={400}
+            className="object-fill"
+          />
+          <div className="absolute flex flex-col size-32 rounded-lg bg-zinc-100 top-20 left-40 border-foreground border-4 overflow-hidden">
             <Image
               src="https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg"
               alt="user"
@@ -38,7 +53,37 @@ export default async function SubdomainPage({
             />
           </div>
         </div>
-        <div className="h-48 w-full bg-muted"></div>
+        <div className="w-full px-40 py-14 space-y-4">
+          <div className="flex justify-between">
+            <div>
+              <h1 className="text-xl font-semibold capitalize">{store.name}</h1>
+              <p className="text-sm text-muted-foreground">
+                {store.description}
+              </p>
+            </div>
+            <div className="flex gap-x-1">
+              <Button variant="link" size="sm">
+                <WhatsApp className="text-white" />
+                WhatsApp
+              </Button>
+              <Button variant="link" size="sm">
+                <Instagram />
+                Instagram
+              </Button>
+            </div>
+          </div>
+          <div className="flex gap-x-2">
+            {store.categories.map((cat) => (
+              <div
+                key={cat.name}
+                className="flex gap-x-2 bg-primary-foreground px-4 py-2 rounded-full border-muted border"
+              >
+                <List className="size-4" />
+                {cat.name}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     );
   } catch (err) {
--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "i.ebayimg.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   async rewrites() {
