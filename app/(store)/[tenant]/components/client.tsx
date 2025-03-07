6c53015234fadb590c51e147ce5e69d5b7f64adb
Fredd <134991353+freddwithy@users.noreply.github.com>
2025-03-07 12:54:40 -0300

    cateogories

--- app/(store)/[tenant]/components/client.tsx
+++ app/(store)/[tenant]/components/client.tsx
@@ -3,6 +3,7 @@ import React, { Suspense } from "react";
 import ProductByCategories from "./products";
 import ProductsSkeleton from "./ui/skeletons/products-skeleton";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import getCategories from "@/actions/get-categories";
 
 interface ClientComponentProps {
   storeId: string;
@@ -11,6 +12,7 @@ interface ClientComponentProps {
 const ProductsClientComponent: React.FC<ClientComponentProps> = async ({
   storeId,
 }) => {
+  const categories = await getCategories({ storeId });
   return (
     <div className="space-y-8">
       <div className="space-y-4">
@@ -31,15 +33,30 @@ const ProductsClientComponent: React.FC<ClientComponentProps> = async ({
           <ScrollBar orientation="horizontal" />
         </ScrollArea>
       </div>
-      <Titles
-        title="Explora nuestros productos"
-        description="Aqui encontraras todos los productos de la tienda"
-      />
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        <Suspense fallback={<ProductsSkeleton numberOfProducts={8} />}>
-          <ProductByCategories storeId={storeId} />
-        </Suspense>
-      </div>
+      {categories.map((cat) => (
+        <div key={cat.id} className="space-y-4">
+          <Titles
+            title={cat.name}
+            description={
+              cat.description
+                ? cat.description[0].toUpperCase() + cat.description.slice(1)
+                : "Categoría de productos"
+            }
+          />
+          <ScrollArea>
+            <div className="flex gap-4">
+              <Suspense fallback={<ProductsSkeleton numberOfProducts={4} />}>
+                <ProductByCategories
+                  storeId={storeId}
+                  categoryId={cat.id}
+                  limit={4}
+                />
+              </Suspense>
+            </div>
+            <ScrollBar orientation="horizontal" />
+          </ScrollArea>
+        </div>
+      ))}
     </div>
   );
 };
--- app/(store)/[tenant]/page.tsx
+++ app/(store)/[tenant]/page.tsx
@@ -1,6 +1,6 @@
 import Instagram from "@/components/icons/instagram";
 import WhatsApp from "@/components/icons/whatsapp";
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import prismadb from "@/lib/prismadb";
 import Image from "next/image";
 import { notFound } from "next/navigation";
@@ -29,12 +29,15 @@ export default async function SubdomainPage({
 
   return (
     <div className="w-full mx-auto max-w-[1080px] h-dvh flex flex-col">
-      <div className="w-full py-14 px-8 space-y-4">
+      <div className="w-full py-14 px-4 space-y-4">
         <div className="flex justify-between items-end">
           <div className="space-y-2">
             <div className="rounded-lg overflow-hidden border aspect-square size-32">
               <Image
-                src="https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg"
+                src={
+                  store.logo ||
+                  "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg"
+                }
                 alt="user"
                 width={128}
                 height={128}
@@ -52,14 +55,20 @@ export default async function SubdomainPage({
               </Suspense>
             </div>
             <div className="flex gap-x-4">
-              <Button variant="link" size="sm" className="px-0">
+              <a
+                className={buttonVariants({ variant: "link", size: "sm" })}
+                href={store.whatsapp || ""}
+              >
                 <WhatsApp className="text-white" />
                 WhatsApp
-              </Button>
-              <Button variant="link" size="sm" className="px-0">
+              </a>
+              <a
+                className={buttonVariants({ variant: "link", size: "sm" })}
+                href={store.instagram || ""}
+              >
                 <Instagram />
                 Instagram
-              </Button>
+              </a>
             </div>
           </div>
           <Cart />
