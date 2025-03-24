69dabbce7fc93e5a0939f21c1984b4841c70755a
Fredd <134991353+freddwithy@users.noreply.github.com>
2025-03-24 18:43:32 -0300

    fixing loader

--- components/general-loader.tsx
+++ components/general-loader.tsx
@@ -1,12 +1,16 @@
 "use client";
 import React from "react";
-import { bouncy } from "ldrs";
-bouncy.register();
+import dynamic from "next/dynamic";
+
+// Dynamically import the actual loader component
+const BouncyLoader = dynamic(() => import("./loaders/bouncy"), {
+  ssr: false,
+});
 
 const GeneralLoader = () => {
   return (
     <div className="w-full h-dvh flex items-center justify-center bg-background flex-col gap-y-4 animate-fade">
-      <l-bouncy size="60" speed="1" color="hsl(var(--foreground))"></l-bouncy>
+      <BouncyLoader />
     </div>
   );
 };
